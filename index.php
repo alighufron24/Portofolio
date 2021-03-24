41eb78e8eb618ae3e598f7c5dfe2262887ec9ef4
unknown <alighufron.ag33@gmail.com>
2021-03-24 10:50:49 +0700

    Proses

---
+++ index.php
@@ -0,0 +1,180 @@
+<?php
+include 'koneksi.php';
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@100&display=swap" rel="stylesheet">
+
+    <title>Portofolio</title>
+    <style>
+      body{
+        font-family: Noto Sans SC;
+        background-color: #D9EFF4;
+      }
+      a b:hover{
+        text-decoration: black underline;
+      }
+      section{
+        background-color: #1B95AF;
+      }
+      .card{
+        border: none;
+      }
+    </style>
+  </head>
+  <body>
+
+    <?php
+
+        $sql = "SELECT * FROM profil";
+        $query = mysqli_query($connect,$sql);
+
+        while($profil = mysqli_fetch_array($query)){
+
+
+      ?>
+    
+    <div id="home"></div>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent fixed-top p-3" style="background-color: #1B95AF;">
+      <div class="container-sm">
+        <a href="https://www.instagram.com/a.ghuf/" class="nav-link  active me-4 fs-5 fw-bold" style="color: black;"><?php echo $profil['nama'] ?></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse-sm navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ms-auto">
+            <a class="nav-link  active me-4" aria-current="page" href="#home"><b>Home</b></a>
+            <a class="nav-link  active me-4" href="#about"><b>About</b></a>
+            <a class="nav-link  active me-4" href="#gallery"><b>Gallery</b></a>
+            <a class="nav-link  active" href="#contact"><b>Contact</b></a>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+
+    <!-- Jumbotron -->
+    
+    <div class="jumbotron-sm jumbotron-fluid mb-5" style="height: 600px; background-color: #1B95AF; background-size: cover; background-position-y: -150px;">
+      <div class="container-sm text-light text-center">
+      
+        <img class="img-thumbnail rounded-circle shadow" src="<?php echo $profil['gambar'] ?>" alt="Ali Ghufron" width="250px" height="250px" style="margin-top: 220px;">
+        <p class="lead text-center mt-2 fw-bold fs-2"><?php echo $profil['nama'] ?></p>
+
+        
+      </div>
+    </div>
+
+    <!-- Waves -->
+    <svg id="about" style="margin-top: -48px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#1B95AF" fill-opacity="1" d="M0,256L24,245.3C48,235,96,213,144,208C192,203,240,213,288,218.7C336,224,384,224,432,197.3C480,171,528,117,576,128C624,139,672,213,720,208C768,203,816,117,864,96C912,75,960,117,1008,149.3C1056,181,1104,203,1152,224C1200,245,1248,267,1296,256C1344,245,1392,203,1416,181.3L1440,160L1440,0L1416,0C1392,0,1344,0,1296,0C1248,0,1200,0,1152,0C1104,0,1056,0,1008,0C960,0,912,0,864,0C816,0,768,0,720,0C672,0,624,0,576,0C528,0,480,0,432,0C384,0,336,0,288,0C240,0,192,0,144,0C96,0,48,0,24,0L0,0Z"></path></svg>
+
+
+    <!-- About -->
+    <div class="container-sm" style="margin-top: px;">
+      <h1 class="text-center fw-bold mb-5 display-5">About</h1>
+      <div class="col-sm justify-content text-center fs-5">
+        <p class="col-sm"><?php echo $profil['about'] ?></p>
+      </div>
+    </div>
+    
+    <?php
+        }
+        ?>
+
+    <!-- Waves 2 -->
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#1B95AF" fill-opacity="1" d="M0,256L24,245.3C48,235,96,213,144,208C192,203,240,213,288,218.7C336,224,384,224,432,197.3C480,171,528,117,576,128C624,139,672,213,720,208C768,203,816,117,864,96C912,75,960,117,1008,149.3C1056,181,1104,203,1152,224C1200,245,1248,267,1296,256C1344,245,1392,203,1416,181.3L1440,160L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"></path></svg>
+
+
+
+    <!-- Gallery -->
+    <span id="gallery"></span>
+   <section class="pb-5">
+    <h1 class="text-center fw-bold mb-5 display-5 text-light">Gallery</h1>
+     <div class="container-sm justify-content-evenly">
+       <div class="row">
+         <div class="col-md-4 mb-3">
+          <div class="card shadow-sm" style="width: 100%;">
+            <img src="img/forest.jpeg" class="card-img-top" alt="">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+         </div>
+         <div class="col-md-4 mb-3">
+          <div class="card shadow-sm" style="width: 100%;">
+            <img src="img/forest.jpeg" class="card-img-top" alt="">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+         </div>
+         <div class="col-md-4 mb-3">
+          <div class="card shadow-sm" style="width: 100%;">
+            <img src="img/forest.jpeg" class="card-img-top" alt="">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+         </div>
+       </div>
+     </div>
+   </section>
+
+
+   <!-- Waves 3 -->
+   <svg style="margin-top: -48px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#1B95AF" fill-opacity="1" d="M0,256L24,245.3C48,235,96,213,144,208C192,203,240,213,288,218.7C336,224,384,224,432,197.3C480,171,528,117,576,128C624,139,672,213,720,208C768,203,816,117,864,96C912,75,960,117,1008,149.3C1056,181,1104,203,1152,224C1200,245,1248,267,1296,256C1344,245,1392,203,1416,181.3L1440,160L1440,0L1416,0C1392,0,1344,0,1296,0C1248,0,1200,0,1152,0C1104,0,1056,0,1008,0C960,0,912,0,864,0C816,0,768,0,720,0C672,0,624,0,576,0C528,0,480,0,432,0C384,0,336,0,288,0C240,0,192,0,144,0C96,0,48,0,24,0L0,0Z"  ></path></svg>
+
+   <!-- Contact -->
+   <br id="contact"><br><br><br>
+   <div class="container-sm">
+    <h1 class="text-center mb-5"><b>Contact</b></h1>
+    <div class="form-group mb-3">
+      <label for="name">Nama</label>
+      <input id="name" class="form-control" type="text" name="nama" placeholder="Masukkan Nama">
+    </div>
+    <div class="form-group mb-3">
+      <label for="email">Alamat Email</label>
+      <input id="email" class="form-control" type="email" name="email" placeholder="name@example.com">
+    </div>
+    <div class="form-group mb-3">
+      <label for="comment">Komentar</label>
+      <textarea id="comment" class="form-control" name="comment" rows="3" placeholder="Komentar disini"></textarea>
+    </div>
+    <input name="kirim" id="kirim" class="btn btn-outline-primary" type="button" value="Kirim" style="width: 100%;">
+   </div>
+
+
+
+   <!-- Footer -->
+   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#1B95AF" fill-opacity="1" d="M0,256L24,245.3C48,235,96,213,144,208C192,203,240,213,288,218.7C336,224,384,224,432,197.3C480,171,528,117,576,128C624,139,672,213,720,208C768,203,816,117,864,96C912,75,960,117,1008,149.3C1056,181,1104,203,1152,224C1200,245,1248,267,1296,256C1344,245,1392,203,1416,181.3L1440,160L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"></path></svg>
+   <footer class="footer position-relative text-center pb-3" style="background-color: #1B95AF;">
+     <div class="text-center fw-bolder text-light">
+       <h4>Created by © Ali Ghufron</h4>
+     </div>
+   </footer>
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
